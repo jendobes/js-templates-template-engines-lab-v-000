389ee86db51c05617d96e07ded1bc359e5412073
Jennifer Dobak <jdobak1@gmail.com>
2018-05-11 13:34:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function postComment() {
   let comment = document.getElementById('commentText').value
   let commenter = document.getElementById('commentName').value
   let commentTemplate = document.getElementById('comment-template').innerHTML
-
+  let templateFn = _.template(commentTemplate)
 }
 
 function createPost() {
